7d11429392bd54f66352a3e489738005267748dc
mikejensen-gh <michael@bidray.com>
2019-01-31 11:47:49 -0800

    Testing: styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,17 @@
 import { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+  background: ${({color}) => color};
+  font-size: ${({huge}) => (huge ? '100px' : '50px')};
+  /* font-size: 50px; */
+
+  span {
+    font-size: 100px;
+  }
+`;
 
 class Page extends Component {
   render() {
@@ -8,6 +19,10 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton huge color="green">
+          Click me
+          <span>💪</span>
+        </MyButton>
         {this.props.children}
       </div>
     );

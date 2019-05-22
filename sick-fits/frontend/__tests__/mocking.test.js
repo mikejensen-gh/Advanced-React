2fb641c84da79d9b54d24dcb4c1eaf79083df788
mikejensen-gh <michael@bidray.com>
2019-05-22 08:44:35 +0200

    Video 56 - basic mocking

--- sick-fits/frontend/__tests__/mocking.test.js
+++ sick-fits/frontend/__tests__/mocking.test.js
@@ -1,3 +1,43 @@
-describe('mocking learnign', () => {
-  it('mocks a reg function', () => {});
+function Person(name, foods) {
+  this.name = name;
+  this.foods = foods;
+}
+
+Person.prototype.fetchFavFoods = function() {
+  return new Promise((resolve, reject) => {
+    // Simulate an API
+    setTimeout(() => resolve(this.foods), 2000);
+  });
+};
+
+describe('mocking learning', () => {
+  it('mocks a reg function', () => {
+    const fetchDogs = jest.fn();
+
+    fetchDogs('snickers');
+
+    expect(fetchDogs).toHaveBeenCalled();
+    expect(fetchDogs).toHaveBeenCalledWith('snickers');
+
+    fetchDogs('hugo');
+
+    expect(fetchDogs).toHaveBeenCalledTimes(2);
+  });
+
+  it('can create a person', () => {
+    const me = new Person('Mike', ['lasagne', 'seitan']);
+
+    expect(me.name).toBe('Mike');
+  });
+
+  it('can fetch foods', async () => {
+    const me = new Person('Mike', ['lasagne', 'seitan']);
+
+    // mock the favFoods function
+    me.fetchFavFoods = jest.fn().mockResolvedValue(me.foods);
+
+    const favFoods = await me.fetchFavFoods();
+
+    expect(favFoods).toContain('lasagne');
+  });
 });
